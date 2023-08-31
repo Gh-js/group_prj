dfd7af0b4a255a2adfc1d4b8eef989b6295558f5
DESKTOP-COJ9858\user <dkrnlqkqn586@naver.com>
2023-08-31 16:21:37 +0900

    관리자 백엔드

---
+++ course_prj/src/manager/InventoryManagerSubWindowEvt.java
@@ -0,0 +1,12 @@
+package manager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InventoryManagerSubWindowEvt implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+}
---
+++ course_prj/src/manager/InventoryManagerTabEvt.java
@@ -0,0 +1,12 @@
+package manager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InventoryManagerTabEvt implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+}
